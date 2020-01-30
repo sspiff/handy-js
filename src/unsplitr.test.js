684fc62502b33ddcba9b3675f857466e210b23ab
sspiff <sspiff@users.noreply.github.com>
2020-01-30 12:55:54 -0500

    unsplitr: add a couple of test cases

--- src/unsplitr.test.js
+++ src/unsplitr.test.js
@@ -2,15 +2,19 @@
 import unsplitr from './unsplitr'
 
 describe('unsplitr', () => {
-  test('joins all but the first n items', () => {
-    expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 2))
-      .toEqual(['foo', 'bar', 'baz;quux'])
-  })
   test('joins all items', () => {
     expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 0))
       .toEqual(['foo;bar;baz;quux'])
   })
+  test('joins all but the first n items', () => {
+    expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 1))
+      .toEqual(['foo', 'bar;baz;quux'])
+    expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 2))
+      .toEqual(['foo', 'bar', 'baz;quux'])
+  })
   test('joins no items', () => {
+    expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 3))
+      .toEqual(['foo', 'bar', 'baz', 'quux'])
     expect(unsplitr('foo;bar;baz;quux'.split(';'), ';', 4))
       .toEqual(['foo', 'bar', 'baz', 'quux'])
   })
