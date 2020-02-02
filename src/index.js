3a4e2d16b3692dc32ce7caac9bbcbdcf2e671f91
sspiff <sspiff@users.noreply.github.com>
2020-02-02 17:57:01 -0500

    handy: add summary to module doc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 
-/** @module @sspiff/handy */
+/**
+ * Handy bits of JavaScript:
+ *
+ * - {@link module:@sspiff/handy.fromPairsDeep fromPairsDeep()}
+ *   constructs objects from key/value pairs.
+ * - {@link module:@sspiff/handy.lruMemoize lruMemoize()}
+ *   is a memoizer with a bounded cache size.
+ * - {@link module:@sspiff/handy.perishable perishable()}
+ *   caches a value with an expiration date.
+ * - {@link module:@sspiff/handy.perishableRetryPromise perishablRetryPromise()}
+ *   retries a rejected `perishable` promise with a backoff delay.
+ * - {@link module:@sspiff/handy.pipe pipe()}
+ *   composes functions by pipelining the output of one function to
+ *   the input of another.
+ * - {@link module:@sspiff/handy.unsplitl unsplitl()}
+ *   undoes a portion of a string split,
+ *   like Python's `string.rsplit()`'s `maxSplit`.
+ * - {@link module:@sspiff/handy.unsplitr unsplitr()}
+ *   undoes a portion of a string split,
+ *   like Python's `string.split()`'s `maxSplit`.
+ *
+ * @module @sspiff/handy
+ */
 
 export {default as fromPairsDeep} from './fromPairsDeep'
 export {default as lruMemoize} from './lruMemoize'
